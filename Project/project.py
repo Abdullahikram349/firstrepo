21c0d72db3c618a7d542c2e7c0e62cb0f5b53723
Abdullahikram349 <abdullahext349@gmail.com>
2025-01-02 19:34:01 +0500

    Update project.py
    Sir i am abdullah i add more things in teacher class...

--- Project/project.py
+++ Project/project.py
@@ -22,19 +22,48 @@ class Student(School):
         return f"id = {self.student_id},name = {self.student_name},father name = {self.student_f_name},phone number = {self.student_phone_number},address = {self.student_address},fee = {self.student_fees}"     
 
 class Teacher(School):
-    def __init__(self,
+    def __init__(
+        self,
+        teacher_id,
         teacher_name,
         teacher_f_name,
-        teacher_phone_name,
+        teacher_phone_number,
+        teacher_email,
         teacher_salary,
-        ):
+        teacher_subject,
+        teacher_experience,
+        teacher_address
+    ):
+        self.teacher_id = teacher_id
         self.teacher_name = teacher_name
         self.teacher_f_name = teacher_f_name
-        self.teacher_phone_number = teacher_phone_name
+        self.teacher_phone_number = teacher_phone_number
+        self.teacher_email = teacher_email
         self.teacher_salary = teacher_salary
+        self.teacher_subject = teacher_subject
+        self.teacher_experience = teacher_experience
+        self.teacher_address = teacher_address
 
-    def getteacher(self):
-        return f" Name: {self.teacher_name}  Father Name: {self.teacher_f_name} Phone Number: {self.teacher_phone_number}"   
+    def getTeacher(self):
+        print("id = " + str(self.teacher_id) + ", name = " + self.teacher_name + ", father name = " + self.teacher_f_name + ", phone = " + self.teacher_phone_number + ", email = " + self.teacher_email + ", subject = " + self.teacher_subject + ", experience = " + str(self.teacher_experience) + " years, salary = " + str(self.teacher_salary) + ", address = " + self.teacher_address)
+
+    def updateSalary(self, new_salary):
+        self.teacher_salary = new_salary
+        print("Salary updated to " + str(self.teacher_salary))
+
+    def promoteTeacher(self):
+        self.teacher_experience = self.teacher_experience + 1
+        print(self.teacher_name + " promoted! Experience is now " + str(self.teacher_experience) + " years.")
+
+    def assignSubject(self, subject):
+        self.teacher_subject = subject
+        print("Assigned subject: " + self.teacher_subject)
+
+    def updateContact(self, phone, email, address):
+        self.teacher_phone_number = phone
+        self.teacher_email = email
+        self.teacher_address = address
+        print("Contact details updated: Phone - " + self.teacher_phone_number + ", Email - " + self.teacher_email + ", Address - " + self.teacher_address)
 
 class Admin(School):
     pass
